67e1c4f7312de5e90e1da888b62c7c12a9933ad9
Fat Halimi <halimifat@gmail.com>
2022-04-20 15:33:47 +0200

    Roles done, create new user, create new admin is done

--- pinkJB-core/Controllers/AccountController.cs
+++ pinkJB-core/Controllers/AccountController.cs
@@ -20,7 +20,7 @@ namespace pinkJB_core.Controllers
             _signInManager = signInManager;
             _context = context;
         }
-        
+
         public IActionResult Login()
         {
             var response = new LoginVM();
@@ -30,18 +30,18 @@ namespace pinkJB_core.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginVM loginVM)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
                 return View(loginVM);
 
             //check user exists in DB
             var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
-            if(user!=null)
+            if (user != null)
             {
                 var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
-                if(passwordCheck)
+                if (passwordCheck)
                 {
-                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false,false);
-                    if(result.Succeeded)
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
                     {
                         return RedirectToAction("Store", "Home");
                     }
@@ -54,7 +54,7 @@ namespace pinkJB_core.Controllers
 
         }
 
-        public IActionResult Register()=>View(new RegisterVM());
+        public IActionResult Register() => View(new RegisterVM());
 
         [HttpPost]
         public async Task<IActionResult> Register(RegisterVM registerVM)
@@ -63,7 +63,7 @@ namespace pinkJB_core.Controllers
                 return View(registerVM);
 
             var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
-            if(user!=null)
+            if (user != null)
             {
                 TempData["Error"] = "This email address is already in use.";
                 return View(registerVM);
@@ -74,8 +74,8 @@ namespace pinkJB_core.Controllers
                 Email = registerVM.EmailAddress,
                 UserName = registerVM.EmailAddress,
             };
-            var newUserResponse = await _userManager.CreateAsync(newUser,registerVM.Password);
-            if(newUserResponse.Succeeded)
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+            if (newUserResponse.Succeeded)
             {
                 await _userManager.AddToRoleAsync(newUser, UserRoles.User);
 
@@ -83,11 +83,49 @@ namespace pinkJB_core.Controllers
             return View("RegisterCompleted");
         }
 
-        
+        //this method registers new admin only when you are already signed in as admin
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAdmin(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+                return View(registerVM);
+
+            var adminUser = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+           // var adminUser = await _userManager.FindByEmailAsync(adminUserEmail);
+            if (adminUser == null)
+            {
+                var newAdminUser = new ApplicationUser()
+                {
+                    FullName = registerVM.FullName,
+                    UserName = registerVM.EmailAddress,
+                    Email = registerVM.EmailAddress,
+                    EmailConfirmed = true
+
+
+                };
+                await _userManager.CreateAsync(newAdminUser, registerVM.Password);
+                await _userManager.AddToRoleAsync(newAdminUser, UserRoles.Admin);
+                await _context.SaveChangesAsync();
+
+            }
+
+            //var newUserResponse = await _userManager.CreateAsync(adminUser, registerVM.Password);
+            //if (newUserResponse.Succeeded)
+            //{
+           //     await _userManager.AddToRoleAsync(adminUser, UserRoles.Admin);
+//
+          //  }
+            return View("RegisterCompleted");
+
+        }
+
+
+
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Store","Home");
+            return RedirectToAction("Store", "Home");
 
         }
     }
